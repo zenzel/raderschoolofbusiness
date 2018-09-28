707abaf070d82518719e7ef927b2f4e4811b126a
Eli Zenz <zenze@msoe.edu>
2018-09-27 20:44:51 -0500

    Created eclipse project

---
+++ milestone_one/src/main.c
@@ -0,0 +1,7 @@
+/**
+ * @file main.c
+ * @author Michael Dougherty <doughertymj@msoe.edu>, Eli Zenz <zenze@msoe.edu>
+ * @date 27 September 2018
+ * @brief Main driver for CE4951 Milestone 1.
+ */
+
