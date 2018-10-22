ee7d0bef272ec4f4fc04b9db54954c8f8699bdf1
Eli Zenz <zenze@msoe.edu>
2018-10-22 18:48:50 -0500

    tits

---
+++ network_interface/inc/rx.h
@@ -0,0 +1,13 @@
+/**
+ * @file rx.h
+ * @author Michael Dougherty <doughertymj@msoe.edu>, Eli Zenz <zenze@msoe.edu>
+ * @date 22 October 2018
+ * @brief Header file for CE4951 receiver.
+ */
+
+#ifndef RX_H_
+#define RX_H_
+
+
+
+#endif /* RX_H_ */
---
+++ network_interface/src/rx.c
@@ -0,0 +1,7 @@
+/**
+ * @file rx.c
+ * @author Michael Dougherty <doughertymj@msoe.edu>, Eli Zenz <zenze@msoe.edu>
+ * @date 22 October 2018
+ * @brief Source file for CE4951 receiver milestone.
+ */
+
